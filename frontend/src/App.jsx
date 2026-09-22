b8b1c5f17029b47ab1de798f6a7813c1764f2e46
gamin <gamin@DESKTOP-T7MROPC>
2026-09-22 14:45:00 +0300

    Create responsive navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1>Mini E-Commerce Platform</h1>
-      <p>Welcome to our online store.</p>
+      <Navbar />
+
+      <main>
+        <h1>Mini E-Commerce Platform</h1>
+        <p>Welcome to our online store.</p>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <nav>
+      <h2>Mini E-Commerce</h2>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <a href="/cart">Cart</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+ 
